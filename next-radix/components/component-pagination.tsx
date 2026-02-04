649cf303f33f99465cfb1c9c1b9c68afd45037c8
shadcn <m@shadcn.com>
2026-02-04 12:49:43 +0400

    fix

--- next-radix/components/component-pagination.tsx
+++ next-radix/components/component-pagination.tsx
@@ -19,7 +19,10 @@ export function ComponentPagination() {
         href="/"
         className="text-muted-foreground hover:text-foreground"
       >
-        Home
+        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="size-4">
+          <title>Radix UI</title>
+          <path fill="currentColor" d="M11.52 24a7.68 7.68 0 0 1-7.68-7.68 7.68 7.68 0 0 1 7.68-7.68V24Zm0-24v7.68H3.84V0h7.68Zm4.8 7.68a3.84 3.84 0 1 1 0-7.68 3.84 3.84 0 0 1 0 7.68Z" />
+        </svg>
       </Link>
       <div className="flex items-center gap-4">
         {prev ? (
